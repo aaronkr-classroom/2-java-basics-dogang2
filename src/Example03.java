c1aeade83cf28faff1475d92b4f270043ed546f4
20211814김동연 <161808977+dogang2@users.noreply.github.com>
2024-03-12 17:26:21 +0900

    save

---
+++ src/Example03.java
@@ -0,0 +1,24 @@
+
+public class Example03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b = 6;
+		short s = 2;
+		int i = 12;
+		iong l = 123456789012345L;
+		
+		
+		System.out.print("\tShort: " + s);
+		System.out.print("\ttInt: " + i);
+		System.out.print("\tLong: " + l);
+		System.out.print("\tFloat: " + f);
+		System.out.print("\tDouble: "+ d);
+		// Double: 1.234567890123456789E9
+		
+		System.out.println("\nChar2: " + c2);
+		char c3 = 89;
+		System.out.println("\nChar3: " + c3);
+	}
+
+}
